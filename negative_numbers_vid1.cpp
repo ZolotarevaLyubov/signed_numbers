97f578634ef865082d68f99067618a113e751539
Lyuba Zolotareva <lyubazolotareva6@gmail.com>
2024-10-11 01:03:36 +0200

    Integers_vid1

--- negative_numbers_vid1.cpp
+++ negative_numbers_vid1.cpp
@@ -9,11 +9,54 @@ class Integer {
         int b;
     
     public:
-    
+        Integer(int m, int n) : a(m), b(n) {}
+        
+        void Cout() const {
+            cout << "Pair: " << a << "\\" << b << endl; 
+        }
+        
+        bool operator == (const Integer& other) const {
+            return (a + other.b) == (other.a + b);
+        }
+        
+        int operator + (const Integer& other) const {
+            return (a + other.a) - (b + other.b);
+        }
+        
+        int operator - (const Integer& other) const {
+            return (a + other.b) - (b + other.a);
+        }
+        
+        int operator * (const Integer& other) const {
+            return (a * other.a + b * other.b) - (a * other.b + b * other.a);
+        }
 
-}
+};
 
 int main() {
 
+    Integer num1(7,3);
+    Integer num2(8,1);
+    
+    num1.Cout();
+    num2.Cout();
+    
+    //equal
+    if(num1 == num2){
+        cout << "equal" <<endl;
+    }
+    else
+        cout << "not equal" <<endl;
+    
+    
+    // +
+    cout << "Num1 + Num2 = " << num1 + num2 <<endl;
+    
+    // -
+    cout << "Num1 - Num2 = " << num1 - num2 <<endl;
+    
+    // *
+    cout << "Num1 * Num2 = " << num1 * num2 <<endl;
+    
     return 0;
 }
